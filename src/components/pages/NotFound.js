b2187533054f485b25621a55fb5ff28da693d418
Daniel C. Landon Jr <daniel.landonjr@gmail.com>
2018-08-31 22:22:27 -0400

    Modified initial files for project

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -6,9 +6,7 @@ const NotFound = () => {
       <h1 className="display-4">
         <span className="text-danger">404</span> Page Not Found
       </h1>
-      <p className="lead">
-        Sorry, that page does not exist or is unavailable at this time.
-      </p>
+      <p className="lead">Sorry, that page does not exist</p>
     </div>
   );
 };
